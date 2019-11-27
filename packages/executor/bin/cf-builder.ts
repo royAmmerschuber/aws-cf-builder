e4b4b37ba4fdccdca8d8f6aa2eeb4e61312ec29c
Roy Ammerschuber <roy.ammerschuber@webgate.biz>
2019-11-27 15:51:49 +0100

    made api friendly

--- packages/executor/bin/cf-builder.ts
+++ packages/executor/bin/cf-builder.ts
@@ -3,24 +3,9 @@ import * as fs from "fs"
 
 import * as commander from "commander"
 
-import { SMap } from "aws-cf-builder-core/general"
-import { StackBackend } from "aws-cf-builder-core/stackBackend"
-import { ResourceError } from "aws-cf-builder-core/general"
 import chalk from "chalk"
 import * as _ from "lodash/fp"
-import * as ts from "ts-node"
-let yaml:typeof import("js-yaml")
-function printErrors(errs:SMap<ResourceError>){
-    _.flow(
-        _.toPairs,
-        _.forEach((v:[string,ResourceError])=>{
-            console.log(v[0]+"\n"+
-                chalk.yellow(v[1].type)+"\n"+
-                v[1].errors.join("\n")
-            )
-        })
-    )(errs)
-}
+import { transform } from ".."
 
 const program:commander.Command & {
     typescript:boolean,
@@ -44,66 +29,30 @@ const program:commander.Command & {
     .option('--transform <transform>',"the transform to apply")
     .parse(process.argv) as any
 
-let err=false
-if(program.typescript){
-    try{
-        const opt:ts.Options={
-            compilerOptions:{
-                typeRoots:[
-                    path.join(__dirname,"../types")
-                ],
-            },
-            transpileOnly:!program.check
-        }
-        require("ts-node").register(opt)
-    }catch(ex){
-        console.log(chalk`{red install ts-node if you want to use the {redBright --typescript flag}}`)
-        err=true
-    }
-}else if(program.check){
-    console.log(chalk`{red if you specify {redBright --check} then you also need to specify {redBright --typescript}}`)
-}
-if(program.yaml){
-    try{
-        yaml=require("js-yaml")
-    }catch(e){
-        console.log(chalk` {red couldnt load module {redBright "js-yaml"}}\n try running {yellow "{green npm install js-yaml}"}`)
-    }
-}
 if(!program.args[0]){
     console.log(chalk.red("please specify the file to compile"))
     program.outputHelp()
-    err=true
-}
-if(program.sam){
-    if(program.transform){
-        console.log(chalk`{red do not specify a {redBright --transform} flag if you are using the {redBright --sam} flag}`)
-        err=true
-    }
-    program.transform="AWS::Serverless-2016-10-31"
-}
-if(err){
     process.exit(1)
 }
+
 require("../globals")
 
 const inPath=path.resolve(program.args[0]);
-const mainModule=new StackBackend(require(inPath))
-const errors=mainModule.checkValid()
-if(_.size(errors)>0){
-    printErrors(errors)
+let outputString:string
+try{
+    outputString=transform(inPath,program)
+}catch(e){
+    if(e instanceof Error){
+        console.log(e.message)
+    }else{
+        console.log(e)
+    }
     process.exit(1)
 }
-const modules=new Set<StackBackend>()
 try{
-    mainModule.prepareQueue(modules)
+    let ending:string
     const moduleName=path.parse(inPath).name
-    let outputJSON:string,ending:string
-    const outputObject=mainModule.generateObject()
-    outputObject.Transform=program.transform
-    outputJSON=JSON.stringify(outputObject,null,program.indent ? 4 : 0)
     if(program.yaml){
-        outputJSON=yaml.safeDump(JSON.parse(outputJSON))
         ending=".yaml"
     }else{
         ending=".json"
@@ -118,7 +67,7 @@ try{
                 console.log("couldnt create folder")
                 process.exit(1)
             }
-            fs.writeFile(path.join(outPath,moduleName+ending),outputJSON,err=>{
+            fs.writeFile(path.join(outPath,moduleName+ending),outputString,err=>{
                 if(err){
                     console.log("couldnt create file")
                     process.exit(1)
@@ -129,7 +78,7 @@ try{
             })
         })
     }else{
-        console.log(outputJSON)
+        console.log(outputString)
     }
 }catch(ex){
     console.log(ex)
---
+++ packages/executor/index.ts
@@ -0,0 +1,76 @@
+import * as path from "path"
+
+import { StackBackend } from "aws-cf-builder-core/stackBackend"
+import chalk from "chalk"
+import * as _ from "lodash/fp"
+import * as tsN from "ts-node"
+import { ResourceError, SMap } from "aws-cf-builder-core/general"
+let yaml: typeof import("js-yaml")
+
+function printErrors(errs: SMap<ResourceError>):string {
+    return _.flow(
+        _.toPairs,
+        _.map((v: [string, ResourceError]) => `${ v[0] }\n${ chalk.yellow(v[1].type) }\n${ v[1].errors.join("\n") }\n`),
+        _.join("\n")
+    )(errs)
+}
+export interface TransformOptions {
+    typescript?: boolean,
+    check?: boolean,
+    indent?: boolean,
+    yaml?: boolean,
+    sam?: boolean,
+    transform?: string
+}
+export function transform(file: string, options: TransformOptions): string {
+    if (options.typescript) {
+        try {
+            const opt: tsN.Options = {
+                compilerOptions: {
+                    typeRoots: [
+                        path.join(__dirname, "../types")
+                    ],
+                },
+                transpileOnly: !options.check
+            }
+            require("ts-node").register(opt)
+        } catch (ex) {
+            throw new Error(chalk`{red couldnt load module {redBright "ts-node"}}\n try running {yellow "{green npm install ts-node}"}`)
+        }
+    } else if (options.check) {
+        throw new Error(chalk`{red if you specify {redBright --check} then you also need to specify {redBright --typescript}}`)
+    }
+    if (options.yaml) {
+        try {
+            yaml = require("js-yaml")
+        } catch (e) {
+            throw new Error(chalk` {red couldnt load module {redBright "js-yaml"}}\n try running {yellow "{green npm install js-yaml}"}`)
+        }
+    }
+    if (options.sam) {
+        if (options.transform) {
+            throw new Error(chalk`{red do not specify a {redBright --transform} flag if you are using the {redBright --sam} flag}`)
+        }
+        options.transform = "AWS::Serverless-2016-10-31"
+    }
+
+    require("./globals")
+
+    const mainModule = new StackBackend(require(file))
+    const errors = mainModule.checkValid()
+    if (_.size(errors) > 0) {
+        throw new Error(printErrors(errors))
+    }
+    const modules = new Set<StackBackend>()
+    mainModule.prepareQueue(modules)
+
+    let outputJSON: string
+    const outputObject = mainModule.generateObject()
+
+    outputObject.Transform = options.transform
+    outputJSON = JSON.stringify(outputObject, null, options.indent ? 4 : 0)
+    if (options.yaml) {
+        outputJSON = yaml.safeDump(JSON.parse(outputJSON))
+    }
+    return outputJSON
+}
