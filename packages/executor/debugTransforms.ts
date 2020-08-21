3312dfecfd85ffd1a3fb9d8d511cd3f66fc33af7
Roy Ammerschuber <roy.ammerschuber@webgate.biz>
2020-08-21 20:12:19 +0200

    rewriting dep-tree generation

--- packages/executor/debugTransforms.ts
+++ packages/executor/debugTransforms.ts
@@ -15,6 +15,14 @@ if(const used before declaration && doesnt depend on non consts){
 
 */
 type depKey=types.Statement|types.VariableDeclaration
+interface DependencyNode{
+    pos:number
+    node:depKey
+    movable:boolean
+    dependencies:Set<DependencyNode>
+    referenceRoots:Set<DependencyNode>
+    references:Set<types.Node>
+}
 const isConstDecl=(stmt:types.Node):stmt is types.VariableDeclaration=>check.isVariableDeclaration(stmt) && stmt.getVariableStatement().getDeclarationKind()==types.VariableDeclarationKind.Const
 const getTopParent=(ref: types.Node):depKey=> ref.getParentWhile(p=>!(
     check.isSourceFile(p) ||
@@ -29,63 +37,88 @@ function getUniqueIdentifier(source:types.SourceFile,base:string){
     }
     return name
 }
+const placeholder=Symbol()
+type placeholder=typeof placeholder
 function transform(source:types.SourceFile){
-    const s=source
-    const dependencyMap=new Map<depKey,{
-        dependencies:Set<depKey>
-        references:Set<depKey>
-    }>()
-    const getOrInitDep=(key:depKey)=>{
-        if(dependencyMap.has(key)) return dependencyMap.get(key)
-        const dep={
-            dependencies:new Set<depKey>(),
-            references:new Set<depKey>()
-        }
-        dependencyMap.set(key,dep)
-        return dep
-    }
+    const dependencyTreeRoots=new Set<DependencyNode>()
+    const allDepNodes=new Map<depKey,DependencyNode|placeholder>()
     //*read dependency tree
-    const handleRefs=(node:depKey&types.ReferenceFindableNode)=>{
-        const nodeDep=getOrInitDep(node)
-        node.findReferencesAsNodes()
-            .filter(ref=>ref.getSourceFile()==s)
-            .map(getTopParent)
-            .filter(ref=>ref!=node)
-            .forEach(stmt=>{
-                nodeDep.references.add(stmt)
-                const refDep=getOrInitDep(stmt as any)
-                refDep.dependencies.add(node as any)
-            })
+    const handleRefs=(node:depKey,root=false):DependencyNode=>{
+        const existent=allDepNodes.get(node)
+        if(existent==placeholder){
+            //TODO recursive
+            throw new Error("recursive")
+        }else if(existent){
+            if(!root) dependencyTreeRoots.delete(existent)
+            return existent
+        }else if(check.isReferenceFindableNode(node)){
+            allDepNodes.set(node,placeholder)
+            const references=node.findReferencesAsNodes()
+                .filter(r=>r.getSourceFile()==source) //only in current file
+                .filter(r=>r.getParent()!=node) //filter out declaration referencing itself for some reason
+            const referenceRoots=references
+                .map(getTopParent)
+                .map(r=>handleRefs(r))
+            const dep:DependencyNode={
+                pos:node.getPos(),
+                node,
+                movable: (
+                    check.isVariableDeclaration(node) &&
+                    node.getVariableStatement().getDeclarationKind()==types.VariableDeclarationKind.Const &&
+                    referenceRoots.every(r=>r.movable)
+                ),
+                dependencies:new Set(),
+                references:new Set(references),
+                referenceRoots:new Set(referenceRoots)
+            }
+            referenceRoots.forEach(r=>r.dependencies.add(dep))
+            allDepNodes.set(node,dep)
+            if(root) dependencyTreeRoots.add(dep)
+            return dep
+        }else{
+            const dep:DependencyNode={
+                pos:node.getPos(),
+                node,
+                movable:false,
+                dependencies:new Set(),
+                referenceRoots:new Set(),
+                references:new Set()
+            }
+            allDepNodes.set(node,dep)
+            if(root) dependencyTreeRoots.add(dep)
+            return dep
+        }
     }
     const handleNamespaceRefs=(node:types.NamespaceDeclaration)=>{
+        //TODO handle namespace scoping
         node.getStatements().forEach(stmt=>{
             if(check.isNamespaceDeclaration(stmt)){
                 handleNamespaceRefs(stmt)
             }else if(check.isVariableStatement(stmt)){
                 stmt.getDeclarations().forEach(dec=>{
-                    handleRefs(dec)
+                    handleRefs(dec,true)
                 })
             }else if(check.isReferenceFindableNode(stmt)){
-                handleRefs(stmt)
+                handleRefs(stmt,true)
             }
         })
     }
-    s.getStatements().forEach(stmt=>{
+    source.getStatements().forEach(stmt=>{
         if(check.isNamespaceDeclaration(stmt)){
             handleNamespaceRefs(stmt)
         }else if(check.isVariableStatement(stmt)){
             stmt.getDeclarations().forEach(dec=>{
-                handleRefs(dec)
+                handleRefs(dec,true)
             })
         }else if(check.isReferenceFindableNode(stmt)){
-            handleRefs(stmt)
+            handleRefs(stmt,true)
         }
     })
     //*write const list
     let insertPos=0
     const reorder=(stmt:types.VariableDeclaration)=>{
-        const name=getUniqueIdentifier(s,stmt.getName())
-        s.insertVariableStatement(insertPos,{
+        const name=getUniqueIdentifier(source,stmt.getName())
+        source.insertVariableStatement(insertPos,{
             declarations:[ {
                 name,
                 initializer:stmt.getInitializer().getText(),
@@ -132,7 +165,7 @@ function transform(source:types.SourceFile){
     }
     dependencyMap
         .forEach((_o,stmt)=>handleDep(stmt))
-    return s.compilerNode
+    return source.compilerNode
 }
 const project=new Project({
     tsConfigFilePath:require.resolve("./testFiles/tsconfig.json")
